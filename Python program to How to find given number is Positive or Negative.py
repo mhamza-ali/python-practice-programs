24a114b23b0278482a35ef4c80062ca1f8cada7e
Muhammad Hamza Ali <hamza4841333@gmial.com>
2021-11-02 01:33:56 +0500

    Initial commit

---
+++ Python program to How to find given number is Positive or Negative.py
@@ -0,0 +1,9 @@
+x = float(input("insert any number: "))
+
+if x > 0:
+    print("This is postive number")
+elif x < 0:
+    print("This is neagtive number")
+else:
+    print("Thr number is zero")
+    
